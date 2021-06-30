86a714e90aedeef210f4e5afd1d2d5c141cd0b33
hugues0 <ntwari.hugues@gmail.com>
2021-06-30 12:36:09 +0200

    ready for submission

--- main.js
+++ main.js
@@ -3,14 +3,40 @@ async function getData() {
     const data = await response.json();
     console.log(data);
     let temp = "";
-    data.forEach((u) => {
+    
+    data.forEach((data) => {
         temp += "<tr>";
-        temp += "<td>"+u.name+"</td>";
-        temp += "<td>" +u.email + "</td>";
-        temp += "<td>" + "<button>Get posts</button>" + "</td></tr>";
+        temp += "<td>" + data.id + "</td>";
+        temp += "<td>"+data.name+"</td>";
+        temp += "<td>" +data.email + "</td>";
+        temp += "<td>" + "<button class= 'btn-get-posts' onclick='getPosts("+data.id+")'>Get posts</button>" + "</td></tr>";
     });
 
     document.getElementById("table-body").innerHTML = temp;
+
 }
 
 getData();
+
+async function getPosts(id) {
+  
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+  const data = await response.json();
+  console.log(data);
+  let temp = "";
+
+  data.forEach((data) => {
+      temp += "<li>" +"<h1>"+ data.title +"</h1>"+ "</li>";
+      temp += "<li>" + "<h2>" + data.body + "</h2>" + "</li>" + "<br>"; 
+  });
+    
+    document.getElementById("content").style.visibility = "hidden";
+    document.getElementById("back").style.visibility = "visible";
+    document.getElementById("target").style.visibility = "visible";
+    document.getElementById("target").innerHTML = temp;
+
+}
+
+
